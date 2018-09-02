b7ae2aeb1aa2e16a2707f0d9bdf4a2e3c88ea1c9
Michael Hoffer <info@michaelhoffer.de>
2018-09-02 14:45:17 +0200

    Update occ-csg.rb

--- Formula/occ-csg.rb
+++ Formula/occ-csg.rb
@@ -1,7 +1,7 @@
 class OccCsg < Formula
   desc "Simple but powerful CSG tool based on OpenCascade (OCE edition)"
   homepage "https://github.com/miho/OCC-CSG"
-  url "https://github.com/miho/OCC-CSG/archive/v0.9.2.tar.gz"
+  url "https://github.com/miho/OCC-CSG/archive/v0.9.3.tar.gz"
   sha256 "84ffde6420326613c45d6740a7e7de7b0e80a9e2f3acce23e167b96dc30eacef"
   depends_on "cmake" => :build
   depends_on "miho/homebrew-tap/occ-dep-for-occ-csg" => :build
@@ -32,11 +32,11 @@ class OccCsg < Formula
   bottle do
     root_url "https://dl.bintray.com/miho/Homebrew-Bottles/"
 
-    rebuild 1
-    sha256 "f0b8e62c8ca29544ec862499cfe229cc595d8f80af5d6ef988ca4802e01e6a87" => :high_sierra
+    #rebuild 1
+    #sha256 "f0b8e62c8ca29544ec862499cfe229cc595d8f80af5d6ef988ca4802e01e6a87" => :high_sierra
     
-    rebuild 2
-    sha256 "c473414e309fe692be625e9c1c72e80e72ffb7df58dd739bff1c83f3e1cb0ece" => :sierra
+    #rebuild 2
+    #sha256 "c473414e309fe692be625e9c1c72e80e72ffb7df58dd739bff1c83f3e1cb0ece" => :sierra
   end
   
   
