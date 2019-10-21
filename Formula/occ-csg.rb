72befe41b24936d7f801e508ee6e540e78a352a9
Michael Hoffer <info@michaelhoffer.de>
2019-10-21 17:18:02 +0200

    Update occ-csg.rb

--- Formula/occ-csg.rb
+++ Formula/occ-csg.rb
@@ -32,11 +32,8 @@ class OccCsg < Formula
   bottle do
     root_url "https://dl.bintray.com/miho/Homebrew-Bottles/"
     
-    sha256 "b31d80801117e8d11fbb30594d8bc74ac52e04998a8659e77d6630d17fedfd22" => :high_sierra
-    
-    rebuild 1
-    sha256 "fed3f775db8bb3a7393fd71bc88edb04f16f9a2857ecb83968d0fd36126d2630" => :sierra
-    
+    cellar :any_skip_relocation
+    sha256 "dbdbc066bbf9bc2e04ee3372cde7fab6330956906761b51759e74ad058a49ed1" => :mojave  
   end
   
   
